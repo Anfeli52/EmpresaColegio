933e08a2b2d4c185ccddfbdda03cb35b8e649982
Anfeli52 <anfeli201111@gmail.com>
2022-10-16 15:57:35 -0500

    Iniciamos las campañas

---
+++ JS/funciones.js
@@ -0,0 +1,8 @@
+function HideAndSeek(){
+    var div = document.getElementById("bell1").value;
+    if(div.style.display=='none'){
+        div.style.display=="block";
+    }else{
+        div.style.display=="none";
+    }
+}
