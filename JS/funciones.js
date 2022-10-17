bd5e09106c76b06eecae4285ca939da8be6cf3a8
Anfeli52 <anfeli201111@gmail.com>
2022-10-16 20:29:36 -0500

    Campañas y Nueva distribución en la relación Carpeta-Archivo Admin-User

--- JS/funciones.js
+++
@@ -1,8 +0,0 @@
-function HideAndSeek(){
-    var div = document.getElementById("bell1").value;
-    if(div.style.display=='none'){
-        div.style.display=="block";
-    }else{
-        div.style.display=="none";
-    }
-}
---
+++ JS/funcionesUser.js
@@ -0,0 +1,40 @@
+function esconderCampana1(){
+    var divBell1 = document.getElementById("idPBell1");
+    if (divBell1.style.display === "none") {
+        divBell1.style.display = "block";
+        divBell1.style.visibility = "visible";
+    } else {
+        divBell1.style.display = "none";
+        divBell1.style.visibility = "hidden";
+    }
+}
+function esconderCampana2(){
+    var divBell1 = document.getElementById("idPBell2");
+    if (divBell1.style.display === "none") {
+        divBell1.style.display = "block";
+        divBell1.style.visibility = "visible";
+    } else {
+        divBell1.style.display = "none";
+        divBell1.style.visibility = "hidden";
+    }
+}
+function esconderCampana3(){
+    var divBell1 = document.getElementById("idPBell3");
+    if (divBell1.style.display === "none") {
+        divBell1.style.display = "block";
+        divBell1.style.visibility = "visible";
+    } else {
+        divBell1.style.display = "none";
+        divBell1.style.visibility = "hidden";
+    }
+}
+function esconderCampana4(){
+    var divBell1 = document.getElementById("idPBell4");
+    if (divBell1.style.display === "none") {
+        divBell1.style.display = "block";
+        divBell1.style.visibility = "visible";
+    } else {
+        divBell1.style.display = "none";
+        divBell1.style.visibility = "hidden";
+    }
+}
