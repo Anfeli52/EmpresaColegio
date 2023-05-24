fe857df51b95d2d4ee3109fbd3f1a839ddabf484
Anfeli52 <anfeli201111@gmail.com>
2023-05-24 07:25:58 -0500

    Condiciones Tabla Usuarios, actualización del diseño de Forgot Password y Update Password

--- PHP/Admin/updateUsers.php
+++ PHP/Admin/updateUsers.php
@@ -12,11 +12,38 @@ if(isset($_POST['btnActualizarUsuarios'])){
     $telefono = $_POST['usuarioTelefono'];
     $tipoUsuario = $_POST['usuarioTipoUsuario'];
 
-    $select = "SELECT * FROM usuario WHERE correo = '$correoCampo'";
-    $result = $conexion->query($select);
+    $selectCorreo = "SELECT * FROM usuario WHERE correo = '$correo'";
+    $selectNombre = "SELECT * FROM usuario WHERE username = '$username'";
 
-    $update = "UPDATE usuario SET correo = '$correo', nombre = '$nombre', apellido = '$apellido', username = '$username', telefono = '$telefono', tipoUsuario = '$tipoUsuario' WHERE correo = '$correoCampo'";
-    $query = mysqli_query($conexion, $update);
+    $queryCorreo = $conexion->query($selectCorreo);
+    $queryNombre = $conexion->query($selectNombre);
+
+    if($correo!="" && $nombre!="" && $apellido!="" && $username!="" && $telefono!="" && $tipoUsuario!=""){
+        if(mysqli_num_rows($queryCorreo)===0){
+            $updateCorreo = "UPDATE usuario SET correo = '$correo', nombre = '$nombre', apellido = '$apellido', telefono = '$telefono', tipoUsuario = '$tipoUsuario' WHERE correo = '$correoCampo'";
+            $return = 1;
+            mysqli_query($conexion, $updateCorreo);
+        }else{
+            $updateCorreo = "UPDATE usuario SET nombre = '$nombre', apellido = '$apellido', telefono = '$telefono', tipoUsuario = '$tipoUsuario' WHERE correo = '$correoCampo'";
+            $return = 0;
+            mysqli_query($conexion, $updateCorreo);
+        }
+
+        if($return===1){
+            $correoActualizado = $correo;
+        }else{
+            $correoActualizado = $correoCampo;
+        }
+
+        if(mysqli_num_rows($queryNombre)===0){
+            $updateNombre = "UPDATE usuario SET username = '$username', nombre = '$nombre', apellido = '$apellido', telefono = '$telefono', tipoUsuario = '$tipoUsuario' WHERE correo = '$correoActualizado'";
+            mysqli_query($conexion, $updateNombre);
+        }else{
+            $updateNombre = "UPDATE usuario SET nombre = '$nombre', apellido = '$apellido', telefono = '$telefono', tipoUsuario = '$tipoUsuario' WHERE correo = '$correoActualizado'";
+            mysqli_query($conexion, $updateNombre);
+        }
+    }
+
     header('location: usersAdminPage.php');
 }else{
     header('location: usersAdminPage.php');
